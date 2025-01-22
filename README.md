158f99b2f3eb673e725eb421d37446c9738cc5a3
Nikolas Siccha <nikolassiccha@gmail.com>
2025-01-22 17:18:05 +0100

    Built site for gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# BayesianSurvival.jl
--- ext/DynamicHMCExt.jl
+++
@@ -1,8 +0,0 @@
-module DynamicHMCExt
-using BayesianSurvival, DynamicHMC, StanBlocks
-
-BayesianSurvival.fit_survival_model(rng, lpdf; n_draws=1000, kwargs...) = begin 
-    mcmc_with_warmup(rng, StanBlocks.with_gradient(lpdf), n_draws; kwargs...)
-end
-
-end
